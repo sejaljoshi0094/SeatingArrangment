3fa91dbba25b9815b083ac91d0bf9b394892244e
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-01-30 16:43:10 +0530

    notices hi message box

--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hi");
+            MessageBox.Show("Hi changes noticed");//ok thank you
         }
     }
 }
